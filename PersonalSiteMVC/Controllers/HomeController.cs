0590c0977eb4533707135177e812b1133d9cfa86
Stacy Toney <stacy.toney@protonmail.com>
2020-12-01 20:05:43 -0600

    projects converted

--- PersonalSiteMVC/Controllers/HomeController.cs
+++ PersonalSiteMVC/Controllers/HomeController.cs
@@ -31,5 +31,10 @@ namespace PersonalSiteMVC.Controllers
         {
             return View();
         }
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
     }
 }
