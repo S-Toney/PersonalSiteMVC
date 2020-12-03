7cd1ec8255fc863e78190a28e07f7689219a83b8
Stacy Toney <stacy.toney@protonmail.com>
2020-12-03 11:56:03 -0600

    html modified and model added

--- PersonalSiteMVC/Controllers/HomeController.cs
+++ PersonalSiteMVC/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Mail;
+using System.Net;
+using PersonalSiteMVC.Models;
 
 namespace PersonalSiteMVC.Controllers
 {
@@ -25,6 +28,52 @@ namespace PersonalSiteMVC.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+            
+            string emailBody = $"You have recieved an email from {cvm.firstName} {cvm.lastName} <br /><br />" +
+                $"With {cvm.Company} / {cvm.Position}<br /><br />" +
+                $"About {cvm.Subject}<br /><br />  with the following message: <br /> <br /> {cvm.Message}" +
+                $"Please respond to {cvm.Email}";
+
+            MailMessage msg = new MailMessage
+            (
+                //From
+                "no-reply@tessatoney.com",
+                //To (Where the actual message is sent)
+                "stacy.toney@protonmail.com",
+                //Subject
+                "Email from tessatoney.com",
+                //Body
+                emailBody
+            );
+
+            msg.IsBodyHtml = true; 
+            SmtpClient client = new SmtpClient("mail.tessatoney.com");
+            client.Credentials = new NetworkCredential("no-reply@tessatoney.com", "20Cen$us20");//change to ****** before pushing to GitHub but use password for deploy
+
+            try
+            {
+                
+                client.Send(msg);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = $"Sorry, something went wrong. Error message : {ex.Message}<br />{ex.StackTrace}";
+                return View(cvm);
+            }
+
+            return View("EmailConfirmation", cvm);
+
         }
 
         public ActionResult Links()
---
+++ PersonalSiteMVC/Models/ContactViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalSiteMVC.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "* Name is required")]
+        public string firstName { get; set; }
+
+        [Required(ErrorMessage = "* Name is required")]
+        public string lastName { get; set; }
+
+        public string Company { get; set; }
+
+        public string Position { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "* Message is required")]
+        [UIHint("MultilineText")]//change frm a singel line textbox to multiline
+        public string Message { get; set; }
+
+    }
+}
