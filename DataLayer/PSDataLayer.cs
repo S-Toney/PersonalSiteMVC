35bde74cdc5cefa856ff1a350d4d0cc29e21a287
Stacy Toney <stacy.toney@protonmail.com>
2022-07-22 00:12:15 -0500

    Added DataLayer; added ErrorMessageWriter, added error logging in Global.asax.cs, Connection string needs connected to DB

---
+++ DataLayer/PSDataLayer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace DataLayer
+{
+    public class PSDataLayer
+    {
+        public string ApplicationName = "PersonalSiteMVC";
+        private string ConnectionString = ConfigurationManager.ConnectionStrings["ApplicationConnectionString"].ConnectionString;
+        
+        protected SqlConnection CreateConnection()
+        {
+            SqlConnection con = new SqlConnection(ConnectionString);
+            return con;
+        }
+
+        #region Error Tracking
+        public void ErrorMessageWriter(Exception ex, string messageURL, string serverName, string app)
+        {
+            SqlConnection con = CreateConnection();
+            using (con)
+            {
+                SqlCommand cmd = new SqlCommand("AddErrorDetailsToDB", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@ApplicationName", app);
+                cmd.Parameters.AddWithValue("@ServerName", serverName);
+                cmd.Parameters.AddWithValue("@URL", messageURL);
+                cmd.Parameters.AddWithValue("@MessageDate", DateTime.Now);
+                if (ex.Source == null)
+                {
+                    string source = "Originally set to null";
+                    cmd.Parameters.AddWithValue("@Source", source);
+                }
+                else
+                {
+                    cmd.Parameters.AddWithValue("@Source", ex.Source);
+                }
+                cmd.Parameters.AddWithValue("@Message", ex.Message);
+                if (ex.InnerException != null)
+                {
+                    cmd.Parameters.AddWithValue("@Stack", ex.InnerException.ToString());
+                }
+                else
+                {
+                    if (ex.StackTrace == null)
+                    {
+                        string stack = "Originally set to null";
+                        cmd.Parameters.AddWithValue("@Stack", stack);
+                    }
+                    else
+                    {
+                        cmd.Parameters.AddWithValue("@Stack", ex.StackTrace);
+                    }
+                }
+                try
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();            }
+                catch (Exception ex1)
+                {
+                    ErrorMessageWriter(ex1, "PSDataLayer.cs", "n/a", app);
+                    throw;
+                }
+            }           
+        }
+        #endregion
+    }
+}
--- PersonalSiteMVC/Global.asax.cs
+++ PersonalSiteMVC/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,7 @@ namespace PersonalSiteMVC
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        string app = "PersonalSiteMVC";
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -17,5 +19,16 @@ namespace PersonalSiteMVC
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception ex = Server.GetLastError();
+            if (ex != null)
+            {
+                PSDataLayer psdl = new PSDataLayer();
+                psdl.ErrorMessageWriter(ex, Request.Url.PathAndQuery, Environment.MachineName, psdl.ApplicationName);
+            }
+        }
     }
 }
+//TODO Add connection string to web.config and appUser to DB Users
