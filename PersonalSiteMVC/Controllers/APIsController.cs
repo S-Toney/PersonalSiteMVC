4380dc4167dbcbd027b6a20ce0cf604410569665
Stacy Toney <stacy.toney@protonmail.com>
2022-07-26 16:16:12 -0500

    Starting Zip Code API

---
+++ PersonalSiteMVC/Controllers/APIsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalSiteMVC.Controllers
+{
+    public class APIsController : Controller
+    {
+        // GET: APIs
+        public ActionResult ZipCode()
+        {
+            return View();
+        }
+    }
+}
